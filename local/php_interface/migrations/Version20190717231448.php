377811f86784df15adb918f138f2ef0f1e9d000d
Viktor Kanishev <kanishevv.dev@gmail.com>
2019-07-18 00:04:53 +0300

    Реализована миграция для добавления базового типа цен

---
+++ local/php_interface/migrations/Version20190717231448.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Sprint\Migration;
+
+use Bitrix\Main\Loader;
+use CCatalogGroup;
+
+Loader::IncludeModule('catalog');
+
+class Version20190717231448 extends Version
+{
+
+    protected $description = "Создаем бозовый вид цен";
+
+    public function up()
+    {
+        
+        $priceId = CCatalogGroup::Add([
+            "BASE" => "Y",
+            "NAME" => "retail",
+            "USER_GROUP" => [1],
+            "USER_GROUP_BUY" => [2], 
+            "USER_LANG" => [
+                "ru" => "Розничная1",
+                "en" => "Retail"  
+            ]
+        ]);
+
+        if ($priceId <= 0) {
+            $this->outError('Ошибка добавления типа цен!');
+        } else {
+            $this->outSuccess('Базовый тип цен добавлен!');
+        }
+        
+    }
+
+    public function down()
+    {
+
+        $dbPriceType = CCatalogGroup::GetList(["NAME" => "retail"]);
+        $priceType = $dbPriceType->Fetch();
+        if ($priceType) {
+            CCatalogGroup::Delete($priceType["ID"]);
+            $this->outSuccess('Тип цен удален с идентификатором ' . $priceType["ID"] . ' удален!');
+        } else {
+            $this->outError('Типа цен с идентификатором ' . $priceType["ID"] . ' не существует!');
+        }
+
+    }
+
+}
